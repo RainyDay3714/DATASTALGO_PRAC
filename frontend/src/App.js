d49a73f3b7bd5b436f7fc752d6a1b8e35d7fe563
San Rain <mgsanjuan@student.hau.edu.ph>
2026-02-01 19:53:29 +0800

    commit 5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { Container, Form } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import ProductScreen from "./screens/ProductScreen";
