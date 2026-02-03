f92b7953770d51ebedce6f7a2c42d68d5b0c24dc
San Rain <mgsanjuan@student.hau.edu.ph>
2026-02-03 18:14:23 +0800

    commit 7

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
+from myapp.products import products
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+@api_view(['GET'])
 def getRoutes(request):
-    return JsonResponse('Hello', safe=True)
+    routes = [
+        '/api/products/',
+        '/api/products/create/',
+
+        '/api/products/upload/',
+
+        '/api/products/<id>/reviews/',
+
+        '/api/products/top/',
+        '/api/products/<id>/',
+
+        '/api/products/delete/<id>/',
+        '/api/products/<update>/<id>/',
+    ]
+    return Response(routes)
+
+@api_view(['GET'])
+def getProducts(request):
+    return Response(products)
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    product = None
+    for i in products:
+        if i['_id'] == pk:
+            product = i
+            break
+    return Response(product)
